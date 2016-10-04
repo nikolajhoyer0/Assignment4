5698e44bc04cb66b0c0917843d896c536c07bcf6
Andrew Tristan Parli <drewparli@gmail.com>
2016-10-04 14:24:01 +0200

    made project template

---
+++ src/erc.erl
@@ -0,0 +1,17 @@
+-module(erc).
+-export([start/0,
+         connect/2,
+         chat/2,
+         history/1,
+         filter/3,
+         plunk/2,
+         censor/2]).
+
+
+%start().
+%connect(Serve, Nick).
+%chat(Server, Cont).
+%history(Server).
+%filter(Sever, Method, P).
+%plunk(Server, Nick).
+%censor(Server, Words).
