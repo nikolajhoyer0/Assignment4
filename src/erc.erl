3f65963a6dda39ec3486854ea2741f2a7bec54e4
Andrew Tristan Parli <drewparli@gmail.com>
2016-10-04 23:34:35 +0200

    just a normal backup

--- src/erc.erl
+++ src/erc.erl
@@ -7,8 +7,26 @@
          plunk/2,
          censor/2]).
 
+% A relay chat server API in erlang (ERC).
 
-%start().
+% For starting an ERC server.
+% Returns {ok, Server} on success or {error, Reason} if an error occurs.
+start() -> spawn(fun loop/0).
+
+% Just a general template right now... does not do anything really.
+loop() ->
+  receive
+    {From, Request} ->
+      From ! {self(), ComputeResult(Request)},
+      loop();
+
+    {From, Other} ->
+      From ! {self(), {error, Other}},
+      loop()
+  end.
+
+
+
 %connect(Serve, Nick).
 %chat(Server, Cont).
 %history(Server).
