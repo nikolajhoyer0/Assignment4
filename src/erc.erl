f523120778395bb18cecb1194d3c3f4ba5a31548
Andrew Tristan Parli <drewparli@gmail.com>
2016-10-06 11:45:21 +0200

    just a normal backup

--- src/erc.erl
+++ src/erc.erl
@@ -7,16 +7,96 @@
          plunk/2,
          censor/2]).
 
-% A relay chat server API in erlang (ERC).
+% Objective
 
-% For starting an ERC server.
-% Returns {ok, Server} on success or {error, Reason} if an error occurs.
-start() -> spawn(fun loop/0).
+% This assignment is about implementing a relay chat server in Erlang (ERC). A
+% relay chat server consists of a central server that clients can connect to,
+% and then send messages which are relayed (broadcast) to other connected
+% clients. An often seen problem with relay chat servers is that when the number
+% of users goes up, various mechanisms are needed for reducing the noise (the
+% number of messages) in the network, the most basic one is the notion of
+% filters.
+
+% The server will keep track of which clients are connected, and for each
+% client, possibly, a filter. When a client sends a message, it is relayed to
+% other connected clients. Each client is registered with a nickname.
+
+% A message is a pair {Nick, Cont} where Nick is the nickname of the client who
+% sent the message, and Cont is the content of the message.
+
+
+
+%%%
+%%% API: A relay chat server.
+%%%
+
+
+% start() for staring an ERC server. Returns {ok, Server} on success or
+% {error, Reason} if some error occurred.
+start() -> spawn(fun() -> loop()).
+
+
+% connect(Server, Nick) for connecting to an ERC server, with the nickname Nick
+% which should be an atom. Returns {ok, Ref} and adds the client to the server
+% if no other client is connected using that nickname. Ref is a unique reference
+% used for identifying messages from the server to the client. Otherwise, if the
+% nickname Nick is taken it should return {error, Nick, is_taken}.
+
+% When a client is connected it should be ready to receive Erlang messages which
+% are pairs of the form {Ref, Msg} where Ref is the reference returned from
+% connect, and Msg is an ERC message, presumably for showing in some kind of UI.
+connect(Serve, Nick) -> true.
+
+
+% chat(Server, Cont) for sending a message with the content Cont, which should
+% be a string, to all other clients in the room. This function should be
+% non-blocking.
+chat(Server, Cont) -> true.
+
+
+% history(Server) for getting the recent messages (capped at 42 messages) sent
+% at the server. Returns a list of messages ordered so that newest message is
+% the first element in the list and the last element is the oldest message.
+history(Server).
+
+
+% filter(Server, Method, P) for filtering messages before they are sent to the
+% client. Where:
+
+%  (a)  P is predicate that takes a message (that is, a tuple of an atom and a
+%       string) and returns true if the message should be sent to the client,
+%       or false if the message should not be sent to the client;
+
+%  (b)  Method is one of the atoms compose or replace. If Method is compose it
+%       means that P should be composed with the previous filter, Q, (if any)
+%       installed for the client, meaning that both P and Q must return true
+%       for a message to be sent to the client. Otherwise, P should replace
+%       any previous filter (if any) installed for the client.
+filter(Sever, Method, P).
+
+
+% plunk(Server, Nick) add a filter for ignoring any message from Nick. Should
+% be implemented using filter, with the compose method.
+plunk(Server, Nick).
+
+
+% censor(Server, Words) add a filter for ignoring messages containing any word
+% in Words, which should be a list of strings. Should be implemented using
+% filter with the compose method.
+censor(Server, Words).
+
+
+
+%%%
+%%% INTERNAL IMPLEMENTATION
+%%%
 
 % Just a general template right now... does not do anything really.
 loop() ->
   receive
+    % From should be a pid
     {From, Request} ->
+      % send back my own pid
       From ! {self(), ComputeResult(Request)},
       loop();
 
@@ -24,12 +104,3 @@ loop() ->
       From ! {self(), {error, Other}},
       loop()
   end.
-
-
-
-%connect(Serve, Nick).
-%chat(Server, Cont).
-%history(Server).
-%filter(Sever, Method, P).
-%plunk(Server, Nick).
-%censor(Server, Words).
